2d649ae0d1dcdec6c19454cb5c0b44751d7a56c0
AviadSabag <Aviadsabag78@gmail.com>
2020-08-07 10:44:13 +0300

    Initial Commit

---
+++ LocalWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocalWeatherApp
+//
+//  Created by Aviad Sabag on 07/08/2020.
+//  Copyright © 2020 Aviad Sabag. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
